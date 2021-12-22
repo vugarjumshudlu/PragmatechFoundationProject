beb2bfe5778f4cb8e66ec1ff4a03976696451f9b
vugarjumshudlu <vugarjumshudlu@outlook.com>
2021-12-22 20:51:00 +0400

    calculator

---
+++ Works/Javascript/Calculator/calculator.js
@@ -0,0 +1,23 @@
+let a = prompt('a ədədini daxil edin')
+let emr = prompt('Isədiyiniz riyazi əməliyyatı daxil edin')
+let b = prompt('b ədədini daxil edin')
+
+if (emr == '+'){
+    let c = Number(a) + Number(b)
+    alert(c)
+}
+
+if (emr == '-'){
+    let c = Number(a) - Number(b)
+    alert(c)
+}
+
+if (emr == '*'){
+    let c = Number(a) * Number(b)
+    alert(c)
+}
+
+if (emr == '/'){
+    let c = Number(a) / Number(b)
+    alert(c)
+}
